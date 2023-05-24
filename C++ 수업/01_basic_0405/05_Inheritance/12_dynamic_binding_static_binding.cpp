b0eb707f9a19f53ba1e9fcd12bbe8d117fc818ea
Jeongan-Kim <stor9592@naver.com>
2023-05-24 14:33:35 +0900

    0524 update

---
+++ C++ 수업/01_basic_0405/05_Inheritance/12_dynamic_binding_static_binding.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+//함수가 호출될때 함수 주소를 연결해 주는 역할(바인딩)
+/*
+	static binding(정적 바인딩, Early binding) : 컴파일 타임에 호출될 함수가 연결됨. 빠르지만 유연하지 않음.
+	dynamic binding(동적 바인딩, late binding) : 런타임에 호출될 함수가 연결됨. 느리지만 유연.
+*/
+
+using namespace std;
+
+int add(int x, int y) { return x + y; }
+int subract(int x, int y) { return x - y; }
+int multiply(int x, int y) { return x * y; }
+
+int main()
+{
+	//정적 바인딩(있는 그대로 쓸 함수를 호출해서 사용하는 방식)
+	int x, y;
+	cin >> x >> y;
+	int op;
+	cout << "0 : add, 1 : subtract, 2 : multiply" << endl;
+
+	int result;
+	switch(op)
+	{
+	case 0: result = add(x, y); break;
+	case 1: result = subract(x, y); break;
+	case 2: result = multiply(x, y); break;
+	}
+
+	cout << result << endl;
+
+	//동적 바인딩(전부 다 저장해 놓고, 주소 연결 자체가 실행 타임에 됨)
+	//함수 포인터에 넣어 놓고, 필요할떄 가져다 씀
+
+	int(*funcptr)(int, int) = nullptr;
+	//하나의 함수 포인터에 여러 함수를 바꿔 넣어가며 쓸 수 있어서 편리함.
+
+	switch (op)
+	{
+	case 0: funcptr = add; break;
+	case 1: funcptr = subract; break;
+	case 2: funcptr = multiply; break;
+	}
+
+	return 0;
+}
