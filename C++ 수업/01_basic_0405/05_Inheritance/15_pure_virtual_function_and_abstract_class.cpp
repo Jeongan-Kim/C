c9ae1122c2df24c7b8c0e10aedd84ef7a4c9d9bb
Jeongan-Kim <stor9592@naver.com>
2023-05-25 14:29:24 +0900

    0525 update

--- C++ 수업/01_basic_0405/05_Inheritance/15_pure_virtual_function_and_abstract_class.cpp
+++ C++ 수업/01_basic_0405/05_Inheritance/15_pure_virtual_function_and_abstract_class.cpp
@@ -8,57 +8,108 @@
 
 using namespace std;
 
-class animal abstract //추상 클래스. abstract 안 붙여줘도 됨.
+#pragma region abstract class
+	//
+	//class animal abstract //추상 클래스. abstract 안 붙여줘도 됨.
+	//{
+	//	string name;
+	//
+	//public:
+	//	animal(const string& name) : name(name) {}
+	//
+	//	auto getname() const { return name; }
+	//
+	//	virtual void sleep() const final // 자식 클래스들이 공유는 하지만 상속 받았을 때 오버라이딩을 하지는 않을 것(특별히 바뀌지 않을)이면 final을 붙여버림
+	//	{
+	//		cout << "sleep" << endl;
+	//	}
+	//
+	//	virtual void speak() const abstract = 0; //순수 가상 함수.
+	//};
+	//
+	//class cat : public animal
+	//{
+	//public:
+	//	cat(string name) : animal(name) {}
+	//
+	//	void speak() const{ cout << "야옹" << endl; }
+	//};
+	//
+	//class dog : public animal
+	//{
+	//public:
+	//	dog(string name) : animal(name) {}
+	//
+	//	void speak() const { cout << "멍멍" << endl; }
+	//};
+	//
+	//class fox : public animal
+	//{
+	//public:
+	//	fox(string name) : animal(name) {}
+	//};
+	//
+	//int main()
+	//{
+	//	//animal ani("asdfsdad"); //순수 가상함수를 하나라도 가졌으면 객체를 만들 수 없다.
+	//
+	//	cat c("cc");
+	//	dog d("dd");
+	//	//fox f("ff"); //순수 가상 함수를 재정의하지 않았으므로 사용 불가.
+	//
+	//	c.sleep();
+	//	d.sleep();
+	//
+	//	c.speak();
+	//	d.speak();
+	//	return 0;
+	//}
+#pragma endregion
+
+#pragma region Interface
+//파일 에러를 띄우는 구문을 만들어보자
+class IErrorLog //인터페이스니까 I를 붙여서 알려줌
 {
-	string name;
-
 public:
-	animal(const string& name) : name(name) {}
+	virtual ~IErrorLog() = default;
+	virtual bool ReportError(const char* const error) = 0;//순수 가상 함수
+	//virtual bool ReportError(const char* const error) abstract; //이렇게도 나타냄.
+};
 
-	auto getname() const { return name; }
-
-	virtual void sleep() const final // 자식 클래스들이 공유는 하지만 상속 받았을 때 오버라이딩을 하지는 않을 것(특별히 바뀌지 않을)이면 final을 붙여버림
+class FileErrorlog : public IErrorLog
+{
+public:
+	virtual bool ReportError(const char* const error) override
 	{
-		cout << "sleep" << endl;
+		cout << "writting error to a file" << endl;
+		return true;
 	}
-
-	virtual void speak() const abstract = 0; //순수 가상 함수.
 };
 
-class cat : public animal
+class ConsoleErrorlog : public IErrorLog
 {
 public:
-	cat(string name) : animal(name) {}
-
-	void speak() const{ cout << "야옹" << endl; }
+	virtual bool ReportError(const char* const error) override
+	{
+		cout << "writting error to a console" << endl;
+		return true;
+	}
 };
 
-class dog : public animal
+void DoSomething(IErrorLog& log)
 {
-public:
-	dog(string name) : animal(name) {}
-
-	void speak() const { cout << "멍멍" << endl; }
-};
-
-class fox : public animal
-{
-public:
-	fox(string name) : animal(name) {}
-};
+	log.ReportError("Error");
+}
 
 int main()
 {
-	//animal ani("asdfsdad"); //순수 가상함수를 하나라도 가졌으면 객체를 만들 수 없다.
+	//IErrorLog f; //순수 가상함수가 있어서 객체를 만들 수 없음.
+	FileErrorlog fileLog;
+	ConsoleErrorlog consoleLog;
 
-	cat c("cc");
-	dog d("dd");
-	//fox f("ff"); //순수 가상 함수를 재정의하지 않았으므로 사용 불가.
-
-	c.sleep();
-	d.sleep();
-
-	c.speak();
-	d.speak();
+	DoSomething(fileLog);
+	DoSomething(consoleLog);
 	return 0;
 }
+
+#pragma endregion
